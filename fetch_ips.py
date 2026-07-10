7280d053124290bac73f3d7fd1ae7e6c2701d5a4
xfgf0903 <160819463+xfgf0903@users.noreply.github.com>
2026-07-10 12:47:20 +0800

    Update fetch_ips.py

--- fetch_ips.py
+++ fetch_ips.py
@@ -8,7 +8,7 @@ text = resp.text  # 获取页面内容
 # 使用正则表达式匹配IPv4地址
 ips = re.findall(r'\d+\.\d+\.\d+\.\d+', text)
 
-# 将提取到的IP逐行写入 data/ip10.txt 文件
-with open("data/ips.txt", "w") as f:
+# 将提取到的IP逐行写入 data/ipT10.txt 文件
+with open("data/ipT10.txt", "w") as f:
     for ip in ips:
         f.write(ip + "\n")
